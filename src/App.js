189d273baaddced4fe1b206beffcc2947737b22d
Erin <erinedawson@gmail.com>
2018-05-12 18:27:30 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+          <h1 className="App-title">Welcome to myMileageTracker!</h1>
+      
+        <h3>Keep track of how far you run, and average your weekly distance with this handy tool!</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
