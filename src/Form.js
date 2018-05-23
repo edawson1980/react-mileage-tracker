94b49c38fc1613b4fb739fe9f40db7331708e683
Erin <erinedawson@gmail.com>
2018-05-22 23:51:50 -0400

    add 'ref' to Form input, 86 onChange handler.  onSubmit produces desired result

--- src/Form.js
+++ src/Form.js
@@ -11,15 +11,20 @@ class Form extends Component {
 
 
   //function takes an argument 'event' and sets the State of 'this' to whatever is inputted for value
+
   handleChange = (event) => {
-    this.setState({value: event.target.value})
+    {/*this.setState({value: event.target.value})*/}
   }
+
 
   //function responds to the event (in this case 'event' is Submitting the form)
   handleSubmit = (event) =>{
     //preventDefault stops the form from technically submitting (sending a get request somewhere with that info)
-    event.preventDefault()
+    event.preventDefault();
+    this.setState({value: this.refs.miles.value})
     console.log(`You ran ${this.state.value} miles today!  Awesome!`);
+
+
 
   };
 
@@ -38,7 +43,8 @@ class Form extends Component {
             {/*using defaultValue instead of value makes the state of this field mutuable. i.e. allows the
               user to actually type something in. if the field has 'value' instead of 'defaultValue', the
               form field becomes read-only*/}
-            <input type="text" defaultValue={this.state.value}  onInput={this.handleChange}/>
+            {/*<input type="text" defaultValue={this.state.value}  onChange={this.handleChange}/>*/}
+            <input type="text" ref="miles"/>
           </label>
 
           <input type="submit" value="Submit" className='form-submit' />
